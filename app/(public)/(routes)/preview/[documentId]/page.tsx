708517f54a437b18c7c128679c1371ad0d1d4c53
Zohaib Manzoor <zohaibmanzoor1700@gmail.com>
2025-05-05 15:50:04 -0400

    preview fits whole page

--- app/(public)/(routes)/preview/[documentId]/page.tsx
+++ app/(public)/(routes)/preview/[documentId]/page.tsx
@@ -30,7 +30,7 @@ export default function DocumentIdPage({
 
   if (document === undefined)
     return (
-      <div>
+      <div className="min-h-full">
         <Cover.Skeleton />
         <div className="md:max-w-3xl lg:max-w-4xl mx-auto mt-10">
           <div className="space-y-4 pl-8 pt-4">
@@ -46,7 +46,7 @@ export default function DocumentIdPage({
   if (document === undefined) return <div>Not found</div>;
 
   return (
-    <div className="pb-40">
+    <div>
       <Cover preview url={document.coverImage} />
       <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
         <Toolbar preview initialData={document} />
